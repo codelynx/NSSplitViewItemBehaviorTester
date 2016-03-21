f639140c3093ec79cbb124b32b8d28dc8246023f
Kaz Yoshikawa <kaz@digitallynx.com>
2016-03-21 16:34:55 +0900

    initial check-in.

---
+++ NSSplitViewItemBehaviorTester/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSSplitViewItemBehaviorTester
+//
+//  Created by Kaz Yoshikawa on 3/21/16.
+//  Copyright © 2016 Electricwoods LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ NSSplitViewItemBehaviorTester/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  NSSplitViewItemBehaviorTester
+//
+//  Created by Kaz Yoshikawa on 3/21/16.
+//  Copyright © 2016 Electricwoods LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
